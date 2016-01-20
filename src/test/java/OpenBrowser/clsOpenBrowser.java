bd1ca40f97793adb2faad0ac961c293682125819
Sukhwinder Singh <sukhwinders@smartdatainc.net>
2016-01-20 14:12:17 +0530

    New changes with display

--- src/test/java/OpenBrowser/clsOpenBrowser.java
+++ src/test/java/OpenBrowser/clsOpenBrowser.java
@@ -8,13 +8,14 @@ import java.net.URL;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+//import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class clsOpenBrowser {
 	
@@ -28,7 +29,11 @@ public class clsOpenBrowser {
 		//dri=new ChromeDriver();
 		
 		//this.dri=driver;
-		dri=new FirefoxDriver();
+		//dri=new FirefoxDriver();
+		
+		FirefoxBinary firefox = new FirefoxBinary();
+		firefox.setEnvironmentProperty("DISPLAY", ":20");
+		dri = new FirefoxDriver(firefox, null);
 		
 		//DesiredCapabilities capability = DesiredCapabilities.chrome();
 		//WebDriver dri = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
